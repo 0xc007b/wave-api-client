6f1fbefeca373a9347da13cc0c7f38355ee3fd8a
Florent Azonnoudo <florent@weekshipper.com>
2025-05-12 17:09:01 +0000

    Remove deprecated Webhooks API The Webhooks API has been completely removed as it was previously marked for deprecation. This includes removing all associated code, tests, exports, and documentation references.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.4.0] - 2023-05-10
+
+### Removed
+- Webhooks API has been completely removed as it was deprecated
+
 ## [0.3.0] - 2023-04-15
 
 ### Added
@@ -43,6 +48,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Initial documentation
 - Test coverage setup
 
+[0.4.0]: https://github.com/0xc007b/wave-api-client/compare/v0.3.0...v0.4.0
 [0.3.0]: https://github.com/0xc007b/wave-api-client/compare/v0.2.0...v0.3.0
 [0.2.0]: https://github.com/0xc007b/wave-api-client/compare/v0.1.0...v0.2.0
 [0.1.0]: https://github.com/0xc007b/wave-api-client/releases/tag/v0.1.0
--- src/index.ts
+++ src/index.ts
@@ -7,27 +7,6 @@ export * from './api/checkout/types';
 export * from './api/payout/types';
 export * from './api/merchants/types';
 
-// Webhook types - export with care to avoid naming conflicts
-// @deprecated All webhook types are being deprecated and will be removed in a future version
-export {
-  WebhookEvent,
-  WebhookSecurityStrategy,
-  WebhookStatus,
-  WebhookListParams,
-  CreateWebhookRequest,
-  UpdateWebhookRequest,
-  Webhook,
-  VerifySignatureSecretParams,
-  VerifySharedSecretParams,
-  WebhookLastPaymentError,
-  CheckoutSessionCompletedEventData,
-  CheckoutSessionPaymentFailedEventData,
-  MerchantPaymentReceivedEventData,
-  B2BPaymentReceivedEventData,
-  B2BPaymentFailedEventData,
-  WebhookEventPayload,
-} from './api/webhooks/types';
-
 // Export common types
 export { Currency, WaveApiConfig, ErrorCode } from './common/types';
 
